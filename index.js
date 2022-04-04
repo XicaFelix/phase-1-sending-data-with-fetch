59046cb47eec1c2280384b3add674d2182a5d170
Xica Felix <xicasfelix@gmail.com>
2022-04-03 23:18:35 -0400

    commit tree f957d0944d91884eba0d22373ce2984aaf8d79b8

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//       userName, userEmail
+//     }),
+//   };
+  
+  function submitData(name, email){
+      return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name, 
+          email
+        }),
+      }).then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        document.body.innerHTML= object['id'];
+      }).catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        document.body.innerHTML = error.message;
+      });
+  }
